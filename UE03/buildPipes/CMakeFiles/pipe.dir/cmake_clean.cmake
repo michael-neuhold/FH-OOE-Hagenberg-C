5aeedb51752bffad9eb994198ab3930f2b95f0d6
Michael Neuhold <michi.neuhold@gmail.com>
2019-10-28 09:12:16 +0100

    final UE03 v2

---
+++ UE03/buildPipes/CMakeFiles/pipe.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pipe.dir/pipe/pipe.c.o"
+  "CMakeFiles/pipe.dir/pipe/pipelib.c.o"
+  "bin/pipe"
+  "bin/pipe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/pipe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- UE03/pipe/pipe.c
+++ UE03/pipe/pipe.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #include "./pipelib.h"
+
+#define TO_MILLISEC 1000
 
 int main(int argc, char *argv[]){
 
@@ -13,10 +16,16 @@ int main(int argc, char *argv[]){
   int sum = 0;
   
   if(argc == 2) {
-    
+    clock_t start, end = 0;
+    start = clock();
     printf("possibleIter: %d\n", possibleIter(atoi(argv[1]), lengths, counts, n));
+    printf("time: %f ms\n", ((double)((clock() - start)) / CLOCKS_PER_SEC) * TO_MILLISEC);
+    start = clock();
     printf("possibleRec: %d\n", possibleRec(atoi(argv[1]), lengths, counts, n));
+    printf("time: %f ms\n", ((double)((clock() - start)) / CLOCKS_PER_SEC) * TO_MILLISEC);
+    start = clock();
     printf("possibleBack: %d\n", possibleBack(atoi(argv[1]), lengths, counts, n));
+    printf("time: %f ms\n", ((double)((clock() - start)) / CLOCKS_PER_SEC) * TO_MILLISEC);
   } else {
     printf("wrong number of arguments\n");
   }
